7053fdaca18b9adbf9527d0a44f689eb813a31c2
Doreen <Doreen@DESKTOP-S59AJ7A.Home>
2018-01-17 15:38:17 +0200

    settings complete

--- Reversi_ex6/src/gui/SettingsController.java
+++ Reversi_ex6/src/gui/SettingsController.java
@@ -1,12 +1,14 @@
 package gui;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -16,7 +18,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class SettingsController {
+public class SettingsController implements Initializable {
 
 	@FXML
 	private ChoiceBox<String> startPlayer;
@@ -32,10 +34,43 @@ public class SettingsController {
     private Button back;
     
 
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		try{
+			File settingsFile=new File("settings.txt");
+			//if settings exist use them 
+			//if no settings exist use default from fxml
+			if(settingsFile.exists()){
+				BufferedReader readFile = new BufferedReader(new InputStreamReader(new FileInputStream(settingsFile)));
+		        String line = readFile.readLine();
+		        ArrayList<String> settings = new ArrayList<>();
+		        while(line != null) {
+		        	settings.add(line);
+		            line = readFile.readLine();
+		        }
+		        //settings corrupted, use default
+		        if(settings.size()!=4){
+		        	readFile.close();
+		        	return;
+		        }
+		        startPlayer.setValue(settings.get(0));
+		        colorPicker1.setValue(Color.valueOf(settings.get(1)));
+		        colorPicker2.setValue(Color.valueOf(settings.get(2)));
+		        boardSize.setValue(Integer.parseInt(settings.get(3)));
+		        readFile.close();
+			}		
+		} catch (Exception e) {
+            System.out.println("Problem reading from file");
+        }
+		
+	}
+    
+    
 	@FXML
 	protected void submit() {
 		Color color1 = colorPicker1.getValue();
         Color color2 = colorPicker2.getValue();
+        //checks that the colors are different
         if(color1.toString().equals(color2.toString())) {
             this.showAlert("Players Colors cannot be the same!");
             return;
@@ -44,7 +79,7 @@ public class SettingsController {
         String colorPicker1 = color1.toString();
         String colorPicker2 = color2.toString();
         String boardSize = this.boardSize.getValue().toString();
-        
+        //writes the settings to file "settings.txt"
         try {
 			BufferedWriter write = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("settings.txt")));
 			write.write(startPlayer);
@@ -94,5 +129,6 @@ public class SettingsController {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
 	
 }
